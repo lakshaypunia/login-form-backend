827eabf94a40a21dec62a08db1d4e49ee445ad6a
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2025-04-07 00:03:06 +0530

    prisma.ts updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'], // optional, for debugging
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
